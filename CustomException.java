5ea084c36e4fbcf670b0489c3f87e6bb7e3485e9
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-04-18 21:19:33 +0530

    studentnotfound  added exception

--- CustomException.java
+++ CustomException.java
@@ -9,3 +9,9 @@ class DuplicatePRNException extends Exception {
         super(message);
     }
 }
+
+class StudentNotFoundException extends Exception {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
